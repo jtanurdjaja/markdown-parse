97ab96a123a1a48fff5c851778acf91f5e3ac23d
jtanurdjaja <97708996+jtanurdjaja@users.noreply.github.com>
2022-01-20 16:32:23 -0800

    Fixed Test Cases 2-4
    Created a new method to check extensions and to check whether or not the links were written correctly.

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,31 +12,55 @@ if the index of our first opening parenthesis comes before the index
 of our first closinng bracket then we break and return an empty array list.
  */
 
+/*Test Case 2
+Test Case 2 is failing because it is returning an incorrect output
+Although it has the correct syntax for a link, it is referencing an image with a .png
+extension which shouldn't be included in our ouptu
+Try to create an array that contains all possible image extensions
+Iterate through thatt array to check if the links contains one of those extensions
+If it does, don't include that in our output, else add it to our array list
+ */
+
+/*Test Case 3
+Test Case 3 returns an incorrect password
+ */
 public class MarkdownParse {
+    static String[] imageExtensions = {".png", ".jpeg", ".gif", ".csv", ".jpg", ".svg", ".pdf"};
+
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
         // the next )
-        int currentIndex = 0;
-        
+        //  if(!(markdown.indexOf("[") < markdown.indexOf("]") && markdown.indexOf("]")< markdown.indexOf("(") && markdown.indexOf("(") < markdown.indexOf(")"))){ very clean code 100% (LOL)
         int currentIndex = 0;
         int nextOpenBracket = 0;
         int nextCloseBracket = markdown.indexOf("]");
         int openParen = markdown.indexOf("(");
         int closeParen = 0;
-
         while(currentIndex < markdown.length()) {
             if (nextCloseBracket > openParen) break;
             nextOpenBracket = markdown.indexOf("[", currentIndex);
             nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             openParen = markdown.indexOf("(", nextCloseBracket);
             closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            System.out.println("current index is :" + currentIndex);
+
+            if (!checkExtension(markdown.substring(openParen +1, closeParen)) && openParen-nextCloseBracket == 1){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
         }
         return toReturn;
     }
+
+    public static boolean checkExtension(String substring) {
+        for (int i = 0; i < imageExtensions.length; ++i) {
+            if (substring.contains(imageExtensions[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) throws IOException {
 		Path fileName = Path.of(args[0]);
 	    String contents = Files.readString(fileName);
