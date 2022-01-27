66416b9a40394daa2d5314700c1841a3713f6f7a
jtanurdjaja <97708996+jtanurdjaja@users.noreply.github.com>
2022-01-27 15:50:49 -0800

    Testers for getLinks

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import static org.junit.Assert.*;
 import org.junit.*;
 
@@ -5,5 +9,28 @@ public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void readFiles() throws IOException{
+        Path fileName = Path.of("test-file.md");
+	    String contents = Files.readString(fileName);
+        List links = List.of("https://something.com", "some-page.html");
+        assertEquals("checks result", links, MarkdownParse.getLinks(contents));
+
+        Path fileName2 = Path.of("test-file2.md");
+	    String contents2 = Files.readString(fileName2);
+        List links2 = List.of();
+        assertEquals("checks result of test file 2", links2, MarkdownParse.getLinks(contents2));
+
+        Path fileName3 = Path.of("test-file3.md");
+	    String contents3 = Files.readString(fileName3);
+        List links3 = List.of("www.edwin.com");
+        assertEquals("checks result of test file 3", links3, MarkdownParse.getLinks(contents3));
+
+        Path fileName4 = Path.of("test-file4.md");
+	    String contents4 = Files.readString(fileName4);
+        List links4 = List.of("https://CSE15L.com");
+        assertEquals("checks result of test file 4", links4, MarkdownParse.getLinks(contents4));
     }
 }
