f8d49d16f1f730edfc0a698883aae4fb7006c3d5
jtanurdjaja <97708996+jtanurdjaja@users.noreply.github.com>
2022-01-27 16:40:39 -0800

    Testing the new test files

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -33,4 +33,27 @@ public class MarkdownParseTest {
         List links4 = List.of("https://CSE15L.com");
         assertEquals("checks result of test file 4", links4, MarkdownParse.getLinks(contents4));
     }
+
+    @Test
+    public void newTests() throws IOException{
+        Path fileName5 = Path.of("test-file5.md");
+	    String contents5 = Files.readString(fileName5);
+        List links5 = List.of();
+        assertEquals("checks result of test file 5", links5, MarkdownParse.getLinks(contents5));
+
+        Path fileName6 = Path.of("test-file6.md");
+	    String contents6 = Files.readString(fileName6);
+        List links6 = List.of();
+        assertEquals("checks result of test file 6", links6, MarkdownParse.getLinks(contents6));
+
+        Path fileName7 = Path.of("test-file7.md");
+	    String contents7 = Files.readString(fileName7);
+        List links7 = List.of();
+        assertEquals("checks result of test file 7", links7, MarkdownParse.getLinks(contents7));
+
+        Path fileName8 = Path.of("test-file8.md");
+	    String contents8 = Files.readString(fileName8);
+        List links8 = List.of();
+        assertEquals("checks result of test file 8", links8, MarkdownParse.getLinks(contents8));
+    }
 }
