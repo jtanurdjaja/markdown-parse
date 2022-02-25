9728279b2fd2cb35aa06316d97af60e3a8b57749
jtanurdjaja <97708996+jtanurdjaja@users.noreply.github.com>
2022-02-25 11:45:24 -0800

    Fixing the Snippet testers

--- MarkdownTest.java
+++ MarkdownTest.java
@@ -97,21 +97,21 @@ public class MarkdownTest {
     @Test
     public void testSnippet1() throws IOException {
         String contents= Files.readString(Path.of("./snippet1.md"));
-        List<String> expect = List.of("url.com", "google.com", "ucsd.edu");
+        List<String> expect = List.of();
         assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testSnippet2() throws IOException {
         String contents= Files.readString(Path.of("./snippet2.md"));
-        List<String> expect = List.of("a.com", "b.com", "a.com(())", "example.com");
+        List<String> expect = List.of("a.com", "a.com(())", "example.com");
         assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testSnippet3() throws IOException {
         String contents= Files.readString(Path.of("./snippet3.md"));
-        List<String> expect = List.of("https://www.twitter.com", "https://ucsd-cse15l-w22.github.io/", "https://cse.ucsd.edu/", "example.com");
+        List<String> expect = List.of("https://ucsd-cse15l-w22.github.io/");
         assertEquals(expect, MarkdownParse.getLinks(contents));
     }
     
